b17832b59ac88fb97190f3ed574b854bcc8f24cd
MiruGrigoras <miruna.grig@gmail.com>
2023-06-15 05:13:00 +0200

    Add support for Left and Right arrows

--- Program.cs
+++ Program.cs
@@ -4,14 +4,13 @@ namespace TextEditor
     class TextEditor
     {
         int totalRows;
-        int totalColumns;
+        int columnsIntheCurrentRow;
         int cursorRow;
-        int cursorColumn;
 
         public TextEditor()
         {
              cursorRow = totalRows = Console.CursorTop;
-             cursorColumn = totalColumns = Console.CursorLeft;
+             columnsIntheCurrentRow = Console.CursorLeft;
         }
         static void Main(string[] args)
         {
@@ -21,8 +20,6 @@ namespace TextEditor
             do
             {
                 consoleInput = Console.ReadKey();
-
-                //Console.WriteLine(x.Modifiers + " - " + ConsoleModifiers.Shift);
                 switch(consoleInput.Key)
                 {
                     case ConsoleKey.Enter:
@@ -35,27 +32,30 @@ namespace TextEditor
                         editor.ArrowMoveCursor(0, -1);
                         break;
                     case ConsoleKey.LeftArrow:
-                        editor.ArrowMoveCursor(1, 0);
-                        break;
-                    case ConsoleKey.RightArrow:
                         editor.ArrowMoveCursor(-1, 0);
                         break;
-                    default: break;
+                    case ConsoleKey.RightArrow:
+                        editor.ArrowMoveCursor(1, 0);
+                        break;
+                    default: 
+                        editor.checkForNewRowThroughChar();
+                        break;
                 }
-                editor.checkForNewRowThroughChar();
-
-
             } while (consoleInput.Key != ConsoleKey.Escape); //close app on ESC
-            
         }
 
         private void checkForNewRowThroughChar()
         {
             var currentPos = Console.GetCursorPosition();
-            if(currentPos.Top != cursorRow)
+            if (currentPos.Top != cursorRow)
             {
                 cursorRow = currentPos.Top;
+                columnsIntheCurrentRow = 0;
                 totalRows++;
+            }
+            else
+            {
+                columnsIntheCurrentRow++;
             }
         }
 
@@ -63,24 +63,24 @@ namespace TextEditor
         {
             cursorRow += 1;
             totalRows++;
+            columnsIntheCurrentRow = 0;
             Console.SetCursorPosition(0, cursorRow);
         }
 
         private void ArrowMoveCursor(int columnOffset, int rowOffset)
         {
             int newRow = cursorRow + rowOffset;
-            int newColumn = cursorColumn + columnOffset;
+            int newColumn = Console.GetCursorPosition().Left + columnOffset;
             if (NewPositionInsideBounds(newRow, newColumn))
             {
-                cursorColumn = newColumn;
                 cursorRow = newRow;
-                Console.SetCursorPosition(cursorColumn, cursorRow);
+                Console.SetCursorPosition(newColumn, newRow);
             }
         }
 
         private bool NewPositionInsideBounds(int newRow, int newColumn)
         {
-            return newRow <= totalRows && newRow >= 0 && newColumn <= totalColumns && newColumn >= 0;
+            return newRow <= totalRows && newRow >= 0 && newColumn <= columnsIntheCurrentRow && newColumn >= 0;
         }
     }
 }
