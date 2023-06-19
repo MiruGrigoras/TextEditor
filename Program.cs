6daaa64afc9695c47217860811f2c4ccb3453c7d
MiruGrigoras <miruna.grig@gmail.com>
2023-06-19 17:23:07 +0200

    WIP Backspace - problems for multiline paragraph

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
-﻿using System;
-namespace TextEditor
+﻿namespace TextEditor
 {
+    struct InputKey
+    {
+        public ConsoleKeyInfo key { get; init; }
+        public int row { get; init; }
+        public int col { get; init; }
+
+        public InputKey(ConsoleKeyInfo key, int row, int col)
+        {
+            this.key = key;
+            this.row = row;
+            this.col = col;
+        }
+    }
     class TextEditor
     {
         int totalRows;
         int columnsIntheCurrentRow;
         int cursorRow;
+        List<InputKey> inputKeys = new List<InputKey>(); 
         
         // contains each row key and the number of coresponding characters on that row 
         Dictionary<int, int> numberOfColumnsPerRow = new Dictionary<int, int>();
@@ -23,9 +36,10 @@ namespace TextEditor
             do
             {
                 consoleInput = Console.ReadKey();
-                switch(consoleInput.Key)
+                switch (consoleInput.Key)
                 {
                     case ConsoleKey.Enter:
+                        editor.inputKeys.Add(new InputKey(consoleInput, Console.GetCursorPosition().Top, Console.GetCursorPosition().Left));
                         editor.EnterMoveCursor();
                         break;
                     case ConsoleKey.DownArrow:
@@ -40,11 +54,65 @@ namespace TextEditor
                     case ConsoleKey.RightArrow:
                         editor.ArrowMoveCursor(1, 0);
                         break;
+                    case ConsoleKey.Backspace:
+                        editor.HandleBackspace();
+                        break;
                     default: 
+                        editor.inputKeys.Add(new InputKey(consoleInput, Console.GetCursorPosition().Top, Console.GetCursorPosition().Left));
                         editor.CheckForNewRowThroughChar();
                         break;
                 }
+                //editor.RewriteText();
             } while (consoleInput.Key != ConsoleKey.Escape); //close app on ESC
+        }
+
+        private void HandleBackspace()
+        {
+            //Backspace already knows to move the cursor once to the left
+            var (cursorLeft, cursorTop) = Console.GetCursorPosition();
+            int keyIndex = cursorLeft;
+            for(int i=0; i < cursorTop; i++)
+            {
+                keyIndex += numberOfColumnsPerRow[i];
+            }
+            if (keyIndex >= 0)
+            {
+                inputKeys.RemoveAt(keyIndex);
+                if(cursorLeft == numberOfColumnsPerRow[cursorRow]-1)
+                {
+                    Console.Write(' ');
+                    ArrowMoveCursor(-1, 0);
+                }
+                else
+                {
+                    int i, offset = inputKeys.Count - cursorLeft;
+                    for(i = keyIndex; i < inputKeys.Count; i++)
+                        if(inputKeys[i].key.KeyChar == '\r')
+                            Console.Write(" \n");
+                        else Console.Write(inputKeys[i].key.KeyChar);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(cursorLeft, cursorTop);
+                }
+                numberOfColumnsPerRow[cursorRow]--;
+            }
+            
+        }
+
+        private void RewriteText()
+        {
+            Console.Clear();
+            foreach(var key in inputKeys)
+            {
+                Console.Write(key.key.KeyChar);
+            }
+        }
+
+        private void ChangeKey<TKey, TValue>(IDictionary<TKey, TValue> dic,
+                                      TKey fromKey, TKey toKey)
+        {
+            TValue value = dic[fromKey];
+            dic.Remove(fromKey);
+            dic[toKey] = value;
         }
 
         private void CheckForNewRowThroughChar()
@@ -54,6 +122,12 @@ namespace TextEditor
             {
                 cursorRow = Top;
                 columnsIntheCurrentRow = 0;
+                if (numberOfColumnsPerRow.ContainsKey(cursorRow)){
+                    for(int i = numberOfColumnsPerRow.Count-1; i>=cursorRow; i--)
+                    {
+                        ChangeKey(numberOfColumnsPerRow, i, i + 1);
+                    }
+                }
                 numberOfColumnsPerRow.Add(cursorRow, columnsIntheCurrentRow);
                 totalRows++;
             }
@@ -66,6 +140,9 @@ namespace TextEditor
 
         private void EnterMoveCursor()
         {
+
+            columnsIntheCurrentRow++;
+            numberOfColumnsPerRow[cursorRow] = columnsIntheCurrentRow;
             cursorRow += 1;
             totalRows++;
             columnsIntheCurrentRow = 0;
